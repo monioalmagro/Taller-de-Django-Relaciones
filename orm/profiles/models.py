9bef9f58d4073f75e55bf73847e8fba5dfccc918
emi <emazzurque@gmail.com>
2020-05-15 18:18:10 -0300

    models profiles

--- orm/profiles/models.py
+++ orm/profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authors.models import Author
 
-# Create your models here.
+#Relación uno a uno con author
+
+class Profile(models.Model):
+    alias = models.CharField(max_length=50)
+
+    author = models.OneToOneField(Author,on_delete = models.CASCADE, default=None)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.alias
+    
