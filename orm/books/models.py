17dc43c63b06e3a320f6cf1205e6722f3860af79
emi <emazzurque@gmail.com>
2020-05-15 00:44:30 -0300

    migraciones

--- orm/books/models.py
+++ orm/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from authors.models import Author
 
-# Create your models here.
+#Relación uno a muchos con autores
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    #primer argumento el modelo con el que hacemos la relación
+    #on_delete = CASCADE cuando borro un author tambien la reciones con los libros
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    
+    pages = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# La fundación - yo robot - La ultima pregunta
+# it , el resplandor , la torre oscura        
